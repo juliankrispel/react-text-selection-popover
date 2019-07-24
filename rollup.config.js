cbc6d8c7516d947baae3d0670d626139c66e77e5
Julian Krispel-Samsel <julian@reactrocket.com>
2019-07-24 09:17:33 +0200

    bump again

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import babel from "rollup-plugin-babel";
 import commonjs from "rollup-plugin-commonjs";
 import external from "rollup-plugin-peer-deps-external";
-import postcss from "rollup-plugin-postcss";
 import resolve from "rollup-plugin-node-resolve";
 import url from "rollup-plugin-url";
 import globby from "globby";
@@ -36,9 +35,6 @@ const defaultConf = {
   ],
   plugins: [
     external(),
-    postcss({
-      modules: true
-    }),
     url(),
     babel({
       exclude: 'node_modules/**'
